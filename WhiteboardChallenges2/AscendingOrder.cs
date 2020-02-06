7e2ada3413458c6c35f007a276ee495c1a0eabbc
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-06 12:36:02 -0800

    Added new class to order an array in ascending order. Will be trying to write with lowest O(n) possible

---
+++ WhiteboardChallenges2/AscendingOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardChallenges2
+{
+    class AscendingOrder
+    {
+        //Member Variables (HAS A)
+        int[] array;
+
+        //Constructor
+        public AscendingOrder()
+        {
+            array = new int[10];
+        }
+
+        //Member Methods (CAN DO)
+        public void OrderAscending()
+        {
+
+        }
+
+    }
+}
